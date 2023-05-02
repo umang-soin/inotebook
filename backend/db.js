af00db446a3c30bf43ddafde0657315506316f3c
umang-soin <77243505+umang-soin@users.noreply.github.com>
2023-05-02 09:49:50 +0530

    Forst Commit iNotebook

---
+++ backend/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const mongoUri = 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false';
+
+const connectToMongo = async () => {
+    mongoose.connect(mongoUri)
+    .then(() => {console.log("Connected to Mongoose")})
+    .catch((err) => {console.log(err)});
+}
+
+module.exports = connectToMongo;
